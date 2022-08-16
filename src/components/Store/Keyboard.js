e6193363a162e5790c64d4322638de09dbeca408
sameera <48850057+Md-Sameera@users.noreply.github.com>
2022-08-16 18:09:04 +0530

    Update Keyboard.js

--- src/components/Store/Keyboard.js
+++ src/components/Store/Keyboard.js
@@ -5,7 +5,7 @@ import "../Styles.css";
 const Keyboard = () => {
     const [data , useData] = useState([])
   useEffect(()=>{
-      const url = "http://localhost:40025/api/keyboard/";
+      const url = "https://backcomm.herokuapp.com/api/keyboard/";
       fetch(url).then(res => res.json())
       .then(useData)
       .catch(err=>console.log(err))
